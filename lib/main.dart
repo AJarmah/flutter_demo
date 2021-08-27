476abf60279f07939445ed6937f26c585c19818e
Abdullah <abdallahs.jo@gmail.com>
2021-08-27 10:51:23 +0300

    -first commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home:  Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.grey,
+          title: Text('test task'),
+        ),
+        body: Stack(
+
+            children: [
+              Container(
+                color: Colors.white.withOpacity(0.0),
+                child: Text(
+                  'Hey there',
+                  style: TextStyle(
+                   fontSize: 40,
+                   fontWeight: FontWeight.bold
+                  ),
+                ),
+                alignment: Alignment.center,
+              ),
+              InkWell(
+                onTap: (){
+
+                } ,
+                child: Container(
+                  color: Colors.white.withOpacity(0.0),
+
+                ),
+              ),
+                      ]
+        ),
+      ),
+
+    ),);
+}
